de06d80cc0b9ffcd3b038bdb5273fd7879236329
Kuro323 <starcevartem815@gmail.com>
2026-06-28 10:50:36 +0300

    3)Rectangle(OOP)
    Class for create rectangle. And Program.cs for class testing

---
+++ Lessons/Lesson3(OOP)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace education
+{
+    public class Program
+    {
+        /// <summary>
+        /// Точка входа в программу. Демонстрация создания объекта класса Rectangle.
+        /// </summary>
+        /// <param name="args">Аргументы командной строки.</param>
+        public static void Main(string[] args)
+        {
+            //1. Демонстрация работы конструктора по умолчанию (создает прямоугольник 10х10 в координатах 0,0)
+            Rectangle rectangle1 = new Rectangle();
+
+            Console.WriteLine($"Периметр первого пямоугольника = {rectangle1.Perimeter} \nПлощадь первого прямогульника = {rectangle1.Area}");
+
+            //2. Демонстрация работы конструктора c параметрами (создает прямоугольник 40х25 в координатах 5,-3)
+            Rectangle rectangle2 = new Rectangle(5, -3, 40, 25);
+
+            Console.WriteLine($"Периметр второго пямоугольника = {rectangle2.Perimeter} \nПлощадь второго прямогульника = {rectangle2.Area}");
+
+            //3. Тестирование защиты объекта от некорректного состояния
+            try
+            {
+                Console.WriteLine("Попытка создания некорректрного прямогульника:");
+
+                //Передача отрицательного параметра ширины. Код должен прерваться и уйти в блок catch
+                Rectangle rectangle3 = new Rectangle(2, 0.3, -5, 10);
+
+                Console.WriteLine($"Периметр третьего пямоугольника = {rectangle3.Perimeter} \nПлощадь третьего прямогульника = {rectangle3.Area}");
+            }
+            catch (ArgumentException ex)
+            {
+                //Выводим сообщение об ошибке
+                Console.WriteLine($"Ошибка валидации: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Lessons/Lesson3(OOP)/Rectangle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace education
+{
+    /// <summary>
+    /// Класс для представления прямоугольника с заданными координатами и размерами.
+    /// </summary>
+    public class Rectangle
+    {
+        private double _x;
+        private double _y;
+        private double _width;
+        private double _height;
+
+        /// <summary>
+        /// Иницилизация нового экземпляра класса <see cref="Rectangle"/> без параметров со стандартными значениями.
+        /// </summary>
+        public Rectangle()
+        {
+            _x = 0;
+            _y = 0;
+            _width = 10;
+            _height = 10;
+        }
+
+        /// <summary>
+        /// Иницилизация нового экземпляра класса <see cref="Rectangle"/> c параметрами.
+        /// </summary>
+        /// <param name="x">Координата X левого верхнего угла.</param>
+        /// <param name="y">Координата Y левого верхнего угла.</param>
+        /// <param name="width">Ширина прямоугольника (должна быть больше 0).</param>
+        /// <param name="height">Высота прямоугольника (должна быть больше 0).</param>
+        /// <exception cref="ArgumentException">Исключение генерируется, если высота или ширина меньше или равна 0.</exception>
+        public Rectangle(double x, double y, double width, double height)
+        {
+            if(width >= 0 && height >= 0)
+            {
+                _x = x;
+                _y = y;
+                _width = width;
+                _height = height;
+            }
+            else
+            {
+                throw new ArgumentException("Длина и ширина должны быть больше 0");
+            }
+        }
+
+        /// <summary>
+        /// Получение периметра прямоугльника.
+        /// </summary>
+        public double Perimeter { get { return 2 * (_width + _height); } }
+
+        /// <summary>
+        /// Получение площади прямоугольника.
+        /// </summary>
+        public double Area { get { return _width * _height; } }
+    }
+}
